509c63307c1f1a4e577238d67741b2eec3bbd72c
jrg185 <117409560+jrg185@users.noreply.github.com>
2024-10-26 01:15:36 -0400

    Delete setup.sh

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Upgrade pip and setuptools to ensure distutils is available
-pip install --upgrade pip setuptools
-
-# Install the necessary dependencies from requirements.txt
-pip install -r requirements.txt
-
